f99fc9a3936d0dcc87d5a6ea64a02bdd8dc27ef1
Tim Lind <tim.lind@sundsvall.se>
2022-11-29 15:42:34 +0100

    Updated general dependencies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,60 +1,12 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
 module.exports = withBundleAnalyzer({
   output: 'standalone',
   i18n: {
-    locales: ["sv"],
-    defaultLocale: "sv",
+    locales: ['sv'],
+    defaultLocale: 'sv',
   },
-  async headers() {
-    return [
-      {
-        source: "/fonts/Raleway-VariableFont_wght.ttf",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/fonts/Raleway-Regular.ttf",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/fonts/Raleway-Medium.ttf",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/fonts/Raleway-SemiBold.ttf",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/fonts/Raleway-Bold.ttf",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+  experimental: { images: { allowFutureImage: true } },
 });
