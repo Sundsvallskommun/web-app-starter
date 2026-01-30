4ebf7f7ed958479990adf7cd569542a23e6cf491
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2026-01-30 09:51:37 +0100

    api-config improvements

--- backend/src/config/api-config.ts
+++ backend/src/config/api-config.ts
@@ -5,3 +5,10 @@ export const APIS = [
     version: '2.0',
   },
 ] as const;
+
+type ApiName = (typeof APIS)[number]['name'];
+
+export const getApiBase = (name: ApiName) => {
+  const api = APIS.find(api => api.name === name);
+  return `${api?.name}/${api?.version}`;
+};
